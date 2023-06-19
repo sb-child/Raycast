358b87541ad03178962eb4aa0e4c770177c3fbd3
sbchild <sbchild0@gmail.com>
2023-06-19 15:30:53 +0800

    xray exec

--- internal/logic/xray_exec/xray_exec.go
+++ internal/logic/xray_exec/xray_exec.go
@@ -5,9 +5,13 @@ import (
 	"raycast/internal/service"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gproc"
 )
 
 type sXrayExec struct {
+	xrayProgram    string
+	xrayConfigFile string
+	ray            *gproc.Process
 }
 
 func init() {
@@ -18,10 +22,37 @@ func New() *sXrayExec {
 	return &sXrayExec{}
 }
 
+func (x *sXrayExec) xray(ctx context.Context, args []string) *gproc.Process {
+	return gproc.NewProcess(x.xrayProgram, args)
+}
+
+func (x *sXrayExec) testXray(ctx context.Context) {
+	x.ray = x.xray(ctx, []string{"help"})
+	pid, err := x.ray.Start(ctx)
+	if err != nil {
+		g.Log().Warning(ctx, "Failed to start Xray:", err.Error())
+		return
+	}
+	g.Log().Warningf(ctx, "Xray is started, PID is [%d]", pid)
+}
+
+func (x *sXrayExec) runXray(ctx context.Context) {
+
+}
+
+func (x *sXrayExec) configure(ctx context.Context) {
+	x.xrayProgram = g.Config().MustGet(ctx, "raycast.xray", "").String()
+	x.xrayConfigFile = g.Config().MustGet(ctx, "raycast.xrayConfig", "").String()
+}
+
 func (x *sXrayExec) Start(ctx context.Context) {
 	g.Log().Warning(ctx, "[service] Starting XrayExec...")
+	x.configure(ctx)
+	x.testXray(ctx)
+	x.runXray(ctx)
 }
 
 func (x *sXrayExec) Stop(ctx context.Context) {
 	g.Log().Warning(ctx, "[service] Stopping XrayExec...")
+	x.ray.Kill()
 }
